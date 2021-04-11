b4ac701ca5a0b6037d0f8246b2c79a4f915ac053
ahrimy <dlsdjdkfla@gmail.com>
2021-04-12 00:58:09 +0900

    feat: user history 생성 로직 구현

--- RequestEmotionAnalysis/index.js
+++ RequestEmotionAnalysis/index.js
@@ -1,10 +1,10 @@
+const randomBytes = require("crypto").randomBytes;
 const axios = require("axios");
-const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
-const { IamAuthenticator } = require("ibm-watson/auth");
 
 const AWS = require("aws-sdk");
-
 const ddb = new AWS.DynamoDB.DocumentClient();
+const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+const { IamAuthenticator } = require("ibm-watson/auth");
 
 const TRANSLATE_API_CLIENT_ID = process.env.CLIENT_ID;
 const TRANSLATE_API_CLIENT_SECRET = process.env.CLIENT_SECRET;
@@ -25,23 +25,17 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
 });
 
 exports.handler = async (event, context, callback) => {
+  const { userId, sentence, type } = event;
+  if (!userId) {
+    return {
+      statusCode: 403,
+      body: { message: "잘못된 접근입니다." },
+    };
+  }
+
   try {
-    // 사용자가 입력한 문장
-    const { userId, sentence, type } = event;
-
-    if (!userId) {
-      return {
-        statusCode: 403,
-        body: { message: "잘못된 접근입니다." },
-      };
-    }
-
     //번역
-    const translateResult = await axios
-      .post(API_URL, { source: "ko", target: "en", text: sentence }, options)
-      .catch((err) => {
-        errorResponse(err.message, context.awsRequestId, callback);
-      });
+    const translateResult = await axios.post(API_URL, { source: "ko", target: "en", text: sentence }, options);
     const sentenceEn = translateResult.data.message.result.translatedText;
 
     //감정분석
@@ -53,20 +47,12 @@ exports.handler = async (event, context, callback) => {
         },
       },
     };
-    const sentimentResult = await naturalLanguageUnderstanding
-      .analyze(analyzeParams)
-      .then((analysisResults) => {
-        return analysisResults.result;
-      });
+    const sentimentResult = await naturalLanguageUnderstanding.analyze(analyzeParams).then((analysisResults) => {
+      return analysisResults.result;
+    });
 
     //감정분석 결과 정렬
-    const {
-      sadness,
-      joy,
-      fear,
-      disgust,
-      anger,
-    } = sentimentResult.emotion.document.emotion;
+    const { sadness, joy, fear, disgust, anger } = sentimentResult.emotion.document.emotion;
     const emotions = [
       {
         type: "sadness",
@@ -111,22 +97,22 @@ exports.handler = async (event, context, callback) => {
         index = Math.floor(Math.random() * itemList.length);
         sentences.push(itemList.splice(index, 1));
       }
-      result = await getSentence(
-        userEmotion[1],
-        sentences[0].SentenceId,
-        sentences[1].SentenceId
-      );
+      result = await getSentence(userEmotion[1], sentences[0].SentenceId, sentences[1].SentenceId);
       itemList = result.Items;
       for (let i = 0; i < 2; i++) {
         index = Math.floor(Math.random() * itemList.length);
         sentences.push(itemList.splice(index, 1));
       }
     }
+
+    // History 저장
     const data = {
-      Emotion: sentimentResult.emotion.document.emotion,
+      HistoryId: toUrlString(randomBytes(16)),
+      Emotion: { Sadness: sadness, Joy: joy, Disgust: disgust, Fear: fear, Anger: anger },
       Type: type,
+      RequestTime: new Date().toISOString(),
     };
-    recordEmotion(userId, data);
+    await recordHistory(userId, data);
 
     const response = {
       statusCode: 200,
@@ -138,6 +124,10 @@ exports.handler = async (event, context, callback) => {
     errorResponse(err.message, context.awsRequestId, callback);
   }
 };
+
+function toUrlString(buffer) {
+  return buffer.toString("base64").replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+}
 
 function errorResponse(errorMessage, awsRequestId, callback) {
   callback(null, {
@@ -154,11 +144,9 @@ function errorResponse(errorMessage, awsRequestId, callback) {
 
 async function getSentence(type, id1, id2) {
   const params = {
-    // Specify which items in the results are returned.
     FilterExpression: id1
       ? "#contentType = :contentType AND not (Content in (:id1, :id2))"
       : "#contentType = :contentType",
-    // Define the expression attribute value, which are substitutes for the values you want to compare.
     ExpressionAttributeValues: id1
       ? {
           ":contentType": type,
@@ -168,7 +156,6 @@ async function getSentence(type, id1, id2) {
       : {
           ":contentType": type,
         },
-    // Set the projection expression, which the the attributes that you want.
     ExpressionAttributeNames: {
       "#contentType": "Type",
     },
@@ -177,45 +164,35 @@ async function getSentence(type, id1, id2) {
   };
 
   const sentence = ddb
-    .scan(params, function (err, data) {
-      if (err) {
-        console.error(
-          "Unable to read item. Error JSON:",
-          JSON.stringify(err, null, 2)
-        );
-      }
-      return data;
-    })
-    .promise();
+    .scan(params)
+    .promise()
+    .catch((err) => {
+      console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+      throw new Error(err);
+    });
 
   return sentence;
 }
 
-async function recordEmotion(userId, data) {
+async function recordHistory(userId, data) {
   const params = {
     TableName: "Users",
     Key: {
       UserId: userId,
     },
-    UpdateExpression:
-      "set History = list_append(if_not_exists(History, :empty_list), :history), RequestTime = :t",
+    UpdateExpression: "set History = list_append(if_not_exists(History, :empty_list), :history)",
     ExpressionAttributeValues: {
       ":history": [data],
       ":empty_list": [],
-      ":t": new Date().toISOString(),
     },
     ReturnValues: "UPDATED_NEW",
   };
 
   return ddb
-    .update(params, function (err, data) {
-      if (err) {
-        console.error(
-          "Unable to update item. Error JSON:",
-          JSON.stringify(err, null, 2)
-        );
-      }
-      return data;
-    })
-    .promise();
+    .update(params)
+    .promise()
+    .catch((err) => {
+      console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+      throw new Error(err);
+    });
 }
