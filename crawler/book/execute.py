69458d03f66bd2a6c261dc54746c844fb83ea65c
ahrimy <ahrim.yang@gmail.com>
2021-05-11 13:32:38 +0900

    feat: book 크롤러

---
+++ crawler/book/execute.py
@@ -0,0 +1,28 @@
+import sys
+import csv
+import crawling
+
+def main(argv):
+    skip = int(argv[1])
+    with open('final_book_upload_data.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for i in range(0, skip):
+            next(csv_reader)
+        count = 0
+        for row in csv_reader:
+            idx = row["book_idx"]
+            title = row["content_booktitle"]
+            author = row["content_author"]
+            #if line_count == 0:
+            #    print(f'Column names are {", ".join(row)}')
+            #    line_count += 1
+            #    continue
+            print(f'\t{idx} {title}')
+            crawling.search(idx, title)
+            count += 1
+            if count == 50 :
+                break;
+        print(f'Processed {count} lines.')
+
+if __name__ == "__main__":
+    main(sys.argv)
