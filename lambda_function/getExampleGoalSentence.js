26eb8394825b3863b7bdee5c1d9a04c82369a6f8
ahrimy <dlsdjdkfla@gmail.com>
2021-06-03 17:35:15 +0900

    refactor: 불필요한 코드 삭제

--- lambda_function/getExampleGoalSentence.js
+++ lambda_function/getExampleGoalSentence.js
@@ -1,9 +1,14 @@
-//Lambda: RequestSentences
+/**
+ * Lambda: RequestSentences
+ * 예시 문장 요청
+ */
+
 const AWS = require("aws-sdk");
 const ddb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event) => {
   const { userId, historyId } = event;
+
   if (!userId || !historyId) {
     return {
       body: { success: false, message: "userId 또는 historyId가 입력되지 않았습니다." },
@@ -17,6 +22,7 @@ exports.handler = async (event, context, callback) => {
         body: { success: false, message: "History 조회 불가" },
       };
     }
+
     //감정분석 결과 정렬
     const { sadness, joy, fear, disgust, anger } = history.Item.init.emotion;
     const emotions = [
@@ -46,55 +52,37 @@ exports.handler = async (event, context, callback) => {
       return b.score - a.score;
     });
 
-    //DB 에서 문장 조회
-    const userEmotion = [emotions[0].type];
+    // DB 에서 문장 조회
+    const userEmotion = emotions[0].type;
     const sentences = [];
-    let result = await getSentence(userEmotion[0]);
+    let result = await getSentence(userEmotion);
     let itemList = result.Items;
     let index = 0;
-    // if (emotions[0].score > 0.8) {
+
+    // 랜덤으로 4개 선택
     for (let i = 0; i < 4; i++) {
       index = Math.floor(Math.random() * itemList.length);
       sentences.push(itemList.splice(index, 1));
     }
-    // } else {
-    //   userEmotion.push(emotions[1].type);
-    //   for (let i = 0; i < 2; i++) {
-    //     index = Math.floor(Math.random() * itemList.length);
-    //     sentences.push(itemList.splice(index, 1));
-    //   }
-    //   result = await getSentence(userEmotion[1], sentences[0].SentenceId, sentences[1].SentenceId);
-    //   itemList = result.Items;
-    //   for (let i = 0; i < 2; i++) {
-    //     index = Math.floor(Math.random() * itemList.length);
-    //     sentences.push(itemList.splice(index, 1));
-    //   }
-    // }
 
-    const response = {
-      body: { success: true, emotion: userEmotion[0], sentences },
+    return {
+      body: { success: true, emotion: userEmotion, sentences },
     };
-
-    return response;
   } catch (err) {
-    errorResponse(err.message, callback);
+    return {
+      body: {
+        success: false,
+        message: err.message,
+      },
+    };
   }
 };
-
-function errorResponse(errorMessage, callback) {
-  callback(null, {
-    body: {
-      success: false,
-      message: errorMessage,
-    },
-  });
-}
 
 async function getHistory(userId, historyId) {
   const params = {
     Key: {
-      userId: userId,
-      historyId: historyId,
+      userId,
+      historyId,
     },
     ProjectionExpression: "init",
     TableName: "History",
@@ -109,20 +97,12 @@ async function getHistory(userId, historyId) {
     });
 }
 
-async function getSentence(type, id1, id2) {
+async function getSentence(type) {
   const params = {
-    FilterExpression: id1
-      ? "#contentType = :contentType AND not (content in (:id1, :id2))"
-      : "#contentType = :contentType",
-    ExpressionAttributeValues: id1
-      ? {
-          ":contentType": type,
-          ":id1": id1,
-          ":id2": id2,
-        }
-      : {
-          ":contentType": type,
-        },
+    FilterExpression: "#contentType = :contentType",
+    ExpressionAttributeValues: {
+      ":contentType": type,
+    },
     ExpressionAttributeNames: {
       "#contentType": "type",
     },
@@ -130,13 +110,11 @@ async function getSentence(type, id1, id2) {
     TableName: "Sentences",
   };
 
-  const sentence = ddb
+  return ddb
     .scan(params)
     .promise()
     .catch((err) => {
       console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
       throw new Error(err);
     });
-
-  return sentence;
 }
