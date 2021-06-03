5b54b028200bade59db0bf3817480f65b26b2d04
ahrimy <dlsdjdkfla@gmail.com>
2021-06-03 17:26:14 +0900

    refactor: uuid4 사용하여 historyId 생성, 불필요한 코드 삭제

--- lambda_function/postInitSentence.js
+++ lambda_function/postInitSentence.js
@@ -1,11 +1,25 @@
-const randomBytes = require("crypto").randomBytes;
-const axios = require("axios");
+/**
+ * Lambda: RequestAnalysis
+ * 초기 감정 문장 분석 및 History 생성
+ */
 
+const uuid4 = require("uuid4");
+const axios = require("axios");
 const AWS = require("aws-sdk");
 const ddb = new AWS.DynamoDB.DocumentClient();
 const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
 const { IamAuthenticator } = require("ibm-watson/auth");
 
+// watson NLU 설정
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: "2020-08-01",
+  authenticator: new IamAuthenticator({
+    apikey: process.env.SENTIMENT_API_KEY,
+  }),
+  serviceUrl: process.env.SENTIMENT_API_URL,
+});
+
+// PAPAGO Translate 설정
 const TRANSLATE_API_CLIENT_ID = process.env.CLIENT_ID;
 const TRANSLATE_API_CLIENT_SECRET = process.env.CLIENT_SECRET;
 const API_URL = process.env.TRANSLATE_API_URL;
@@ -15,14 +29,6 @@ const options = {
     "X-NCP-APIGW-API-KEY": TRANSLATE_API_CLIENT_SECRET,
   },
 };
-
-const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-  version: "2020-08-01",
-  authenticator: new IamAuthenticator({
-    apikey: process.env.SENTIMENT_API_KEY,
-  }),
-  serviceUrl: process.env.SENTIMENT_API_URL,
-});
 
 exports.handler = async (event, context, callback) => {
   const { userId, sentence, type } = event;
@@ -54,11 +60,9 @@ exports.handler = async (event, context, callback) => {
     const sentimentResult = await naturalLanguageUnderstanding
       .analyze(analyzeParams)
       .then((analysisResults) => {
-        console.log(analysisResults);
         return analysisResults.result;
       })
       .catch((err) => {
-        console.log(err);
         if (err.code === 422) {
           throw new Error("문장이 너무 짧아서 분석할 수 없습니다.");
         }
@@ -68,11 +72,10 @@ exports.handler = async (event, context, callback) => {
     // History 생성
     // 감정분석 결과 저장
     const { sadness, joy, fear, disgust, anger } = sentimentResult.emotion.document.emotion;
-    console.log(sentimentResult.emotion.document.emotion);
     if (sadness + joy + fear + disgust + anger == 0) {
       throw new Error("분석할 수 없는 문장입니다.");
     }
-    const historyId = toUrlString(randomBytes(16));
+    const historyId = uuid4();
     const payload = {
       userId,
       historyId,
@@ -81,41 +84,35 @@ exports.handler = async (event, context, callback) => {
         emotion: { sadness, joy, disgust, fear, anger },
       },
       type,
-      like: { book: [], movie: [] },
-      dislike: { book: [], movie: [] },
+      like: [],
+      dislike: [],
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString(),
     };
-    await recordHistory(payload);
+    await createHistory(payload);
 
-    const response = {
+    return {
       body: { success: true, historyId },
     };
-
-    return response;
   } catch (err) {
-    errorResponse(err.message, context.awsRequestId, callback);
+    return {
+      body: {
+        success: false,
+        message: err.message,
+      },
+    };
   }
 };
 
-function toUrlString(buffer) {
-  return buffer.toString("base64").replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
-}
-
-async function recordHistory(data) {
-  return await ddb
+async function createHistory(data) {
+  return ddb
     .put({
       TableName: "History",
       Item: data,
     })
-    .promise();
-}
-
-function errorResponse(errorMessage, awsRequestId, callback) {
-  callback(null, {
-    body: {
-      success: false,
-      message: errorMessage,
-    },
-  });
+    .promise()
+    .catch((err) => {
+      console.error("Unable to create item. Error JSON:", JSON.stringify(err, null, 2));
+      throw new Error(err);
+    });
 }
